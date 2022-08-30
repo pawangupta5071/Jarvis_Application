ceba08e011b0e97c52d0cdd866bdf2737810d435
pawan gupta <pawangupta5071@gmail.com>
2022-08-30 16:35:57 +0530

    first commit

---
+++ jarvis.py
@@ -0,0 +1,89 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishme():
+    hour = int(datetime.datetime.now().hour)
+    if hour > 0 and hour < 12:
+        speak('Good Morning!')
+
+    elif hour >= 12 and hour < 18:
+        speak('Good Afternoon')
+
+    else:
+        speak('Good Evening')
+
+    speak('Mister pawan, i am Jarvis. Please tell me how may I help you')
+
+
+def takeCommand():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('listening...')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language='en-in')
+        print(f'user said {query}\n')
+
+    except Exception as e:
+        print('say that again please...')
+        return 'None'
+    return query
+
+
+if __name__ == '__main__':
+    wishme()
+    while True:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('Searching wikipedia...')
+            query = query.replace('wikipedia', '')
+            result = wikipedia.summary(query, sentences=2)
+            speak('According to wikipedia')
+            print(result)
+            speak(result)
+
+        elif 'open youtube' in query:
+            webbrowser.open('http://www.youtube.com')
+
+        elif 'open google' in query:
+            webbrowser.open('https://www.google.com/')
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open('http://www.stackoverflow.com')
+
+        elif 'play music' in query:
+            music_dir = 'C:\\Users\\PAWAN\\Music'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime('%H:%M:%S')
+            print(f'the current time is {strTime}')
+            speak(f'sir, the current time is {strTime} ')
+
+        elif 'open vs code' in query:
+            codePath = "C:\\Users\\PAWAN\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'quit' in query:
+            exit()
